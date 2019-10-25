2b6d319058af4b188b23b5caea3abcb9932c6780
sidiousvic <victor6rene@gmail.com>
2019-10-25 20:12:15 +0900

    set up pg + knex, created my first migration

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  db: {
+    client: 'pg',
+    connection: process.env.DB_URL || {
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: process.env.DB_PORT || 5432,
+      database: process.env.DB_NAME || 'cc_api',
+      user: process.env.DB_USER || 'VICSIDIOUS',
+      password: process.env.DB_PASSWORD || 'VICSIDIOUS'
+    }
+  },
+
+  express: {
+    port: process.env.PORT || 3000
+  },
+
+  logger: {
+    format: 'dddd MMMM Do YYYY, h:mm:ss a'
+  }
+};
