7736992ca1b1183c6c572420caa3f0d8a023ad53
sidiousvic <victor6rene@gmail.com>
2019-10-27 10:12:17 +0900

    updated database credentials heroku/postgres

--- config.js
+++ config.js
@@ -2,11 +2,13 @@ module.exports = {
   db: {
     client: 'pg',
     connection: process.env.DB_URL || {
-      host: process.env.DB_HOST || '127.0.0.1',
+      host: process.env.DB_HOST || 'ec2-54-235-163-246.compute-1.amazonaws.com',
       port: process.env.DB_PORT || 5432,
-      database: process.env.DB_NAME || 'taqapi',
-      user: process.env.DB_USER || 'VICSIDIOUS',
-      password: process.env.DB_PASSWORD || 'VICSIDIOUS'
+      database: process.env.DB_NAME || 'd8kc4ea1kq7jb6',
+      user: process.env.DB_USER || 'eazczcapkbjgtk',
+      password:
+        process.env.DB_PASSWORD ||
+        '74ef9bad13609360e0b1506a4e568b2c9b7a7950877e4d6358316ddbb626ef35'
     }
   }
 };
