e8554563a39604a6d8f8f2ded781a6a090303149
sidiousvic <victor6rene@gmail.com>
2019-10-27 11:21:41 +0900

    updated seed data

--- models/migrations/20191025195312_add_taquerias_table.js
+++ models/migrations/20191025195312_add_taquerias_table.js
@@ -15,15 +15,15 @@ exports.up = function(knex, Promise) {
     })
     .then(() => {
       return knex('taquerias').insert([
-        { name: 'El Ratón', specialty: 'tortas de jamón', rating: '🌮🌮🌮' },
-        { name: 'Sr.Taco', specialty: 'tacos de barbacoa', rating: '🌮🌮' },
-        { name: 'Taco Fish', specialty: 'tacos de camarón', rating: '🌮🌮🌮' },
+        { name: 'El Ratón', specialty: 'tortas', rating: '🌮🌮🌮' },
+        { name: 'Sr.Taco', specialty: 'barbacoa', rating: '🌮🌮' },
+        { name: 'Taco Fish', specialty: 'camarón', rating: '🌮🌮🌮' },
         {
-          name: 'Harry Potter y la Orden del Taco',
-          specialty: 'tacos de chile verde',
+          name: 'Harry Potter Y La Orden del Taco',
+          specialty: 'chile relleno',
           rating: '🌮'
         },
-        { name: 'Ricos Tacos', specialty: 'tacos al pastor', rating: '🌮🌮' },
+        { name: 'Ricos Tacos', specialty: 'pastor', rating: '🌮🌮' },
         {
           name: 'Tacos Obregón',
           specialty: 'tacos al pastor',
@@ -35,9 +35,14 @@ exports.up = function(knex, Promise) {
           rating: '🌮🌮🌮'
         },
         { name: 'El Escuadrón', specialty: 'montados', rating: '🌮🌮' },
-        { name: 'La Cubanita', specialty: 'tacos al pastor', rating: '🌮🌮' },
+        { name: 'La Cubanita', specialty: 'al pastor', rating: '🌮🌮' },
         { name: 'Changolin', specialty: 'gringas', rating: '🌮🌮🌮' },
-        { name: 'Cande', specialty: 'tacos de costilla', rating: '🌮' }
+        { name: 'Cande', specialty: 'costilla', rating: '🌮' },
+        { name: "Derek's Tacos", specialty: 'ugh', rating: '🌮' },
+        { name: 'De Carranza', specialty: 'al pastor', rating: '🌮🌮🌮' },
+        { name: 'Chingones', specialty: 'barbacoa', rating: '🌮🌮🌮' },
+        { name: "Ricky's Tacos", specialty: 'al pastor', rating: '🌮🌮' },
+        { name: 'Apestosas', specialty: 'hamburguesas', rating: '🌮🌮🌮' }
       ]);
     });
 };
